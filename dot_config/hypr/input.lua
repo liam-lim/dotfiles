463cc2fe96ac08bc5f00349237160943f6e6dc39
liam-lim <45512390+liam-lim@users.noreply.github.com>
2026-09-24 16:12:01 +0800

    Configure Caps Lock as Escape

--- dot_config/hypr/input.lua
+++ dot_config/hypr/input.lua
@@ -1,6 +1,12 @@
 -- Keep only your personal input overrides here. Uncommented settings below
 -- replace Omarchy's defaults.
 
+hl.config({
+  input = {
+    kb_options = "caps:escape",
+  },
+})
+
 -- Keyboard layout and options.
 -- See https://wiki.hypr.land/Configuring/Basics/Variables/#input
 -- hl.config({
